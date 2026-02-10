474eadb3baa99573eab004531d1ab1763a22f409
syhj0326 <yangheejin746@gmail.com>
2026-02-10 15:09:49 +0900

    도서 신간, 페이지네이션 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,39 +3,50 @@ const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req, res) => {
     // 카테고리 아이디가 있으면?
-    let {category_id} = req.query;
-    if(category_id){
-        let sql = 'SELECT * FROM books WHERE category_id = ?';
-        conn.query(sql, category_id,
-            (err, results) => {
-                if(err) {
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-                if(results.length){
-                    return res.status(StatusCodes.OK).json(results);
-                } else {
-                    return res.status(StatusCodes.NOT_FOUND).end();
-                }
-            }
-        )
-    // 없으면!
-    } else {
-        let sql = 'SELECT * FROM books'
-        conn.query(sql, 
-            (err, results) => {
-                if(err) {
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
-                return res.status(StatusCodes.OK).json(results);
-            }
-        )
+    // limit(page당 도서 수), current_page(현재 페이지 위치)은 프론트엔드에서 전달해줌 URL
+    // offset => (currnet_page - 1) * limit
+    let {category_id, new_books, limit, current_page } = req.query;
+    
+    let offset = limit * (current_page - 1);
+
+    let sql = 'SELECT * FROM books ';
+    let values = []
+    // 카테고리 선택이 되어있는지 신간인지에 따라서 다른 sql 저장
+    if(category_id && new_books) {
+        sql += 'WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 4 YEAR) AND NOW()';
+        values = [category_id];
+    }else if(category_id){
+        sql += 'WHERE category_id = ?';
+        values = [category_id]
+    }else if(new_books){
+        sql += 'WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 4 YEAR) AND NOW()';
     }
+    
+    sql += 'LIMIT ? OFFSET ?';
+    values.push(parseInt(limit), offset)
+
+    // 카테고리별 신간 조회
+    conn.query(sql, values,
+        (err, results) => {
+            if(err) {
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if(results.length){
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+        }
+    )
+
+
 };
 
 const bookDetail = (req, res) => {
     let {id} = req.params;
     
-    let sql = 'SELECT * FROM books WHERE id = ?'
+    let sql = `SELECT * FROM books LEFT JOIN category 
+                ON books.category_id = category.id WHERE books.id=?;`
     conn.query(sql, id,
         (err, results) => {
             if(err) {
