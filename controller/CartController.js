b032ebf13dc7f29ab86f5ddab508ae25c0513f65
yang-hee <yangheejin746@gmail.com>
2026-02-13 11:31:16 +0900

    카테고리 삭제 구현

--- controller/CartController.js
+++ controller/CartController.js
@@ -20,12 +20,37 @@ const addCart = (req, res) => {
 
 // 장바구니 아이템 목록 조회
 const getCart = (req, res) => {
-	
+	const {user_id, selected} = req.body;
+	let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
+				FROM cartItems LEFT JOIN books
+				ON cartItems.book_id = books.id
+				WHERE user_id=? AND cartItems.id IN (?)`;
+	let values = [user_id, selected]
+	conn.query(sql, user_id, ...selected,
+		(err, results) => {
+			if(err) {
+				return res.status(StatusCodes.BAD_REQUEST).end();
+			}
+			return res.status(StatusCodes.OK).json(results);
+		}
+	)
+
 
 }
 
 // 장바구니 아이템 삭제
 const removeCart = (req, res) => {
+	const {id} =req.params;
+
+	let sql = `DELETE FROM cartItems WHERE id=?`
+	conn.query(sql, id,
+		(err, results) => {
+			if(err){
+				return res.status(StatusCodes.BAD_REQUEST).end();
+			}
+			return res.status(StatusCodes.OK).json(results);
+		}
+	)
 
 }
 
