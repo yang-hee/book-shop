63eb6dc2eb4187ea481711b5ae0061cc11452723
yang-hee <yangheejin746@gmail.com>
2026-04-03 10:12:06 +0900

    login

--- auth.js
+++ auth.js
@@ -1,24 +1,25 @@
 // decodeJwt 모듈화
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-dotenv.config()
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const decodeJwt = (req, res) => {
-	try{
-		let jwtToken = req.headers["authorization"];
-		
-		if(receivedJwt) {
-			let decodedJwt = jwt.verify(jwtToken, process.env.PRIVATE_KEY);
-			return decodedJwt;
-		} else {
-			// 토큰이 없는 경우
-			throw new ReferenceError('jwt must be provided');
-		}
-	} catch (err) {
-		console.log(err.name);
-		console.log(err.message);
-		return err
-	}
-}
+  try {
+    let jwtToken = req.headers["authorization"];
+    console.log("jwtToken?", jwtToken);
+    if (jwtToken) {
+      console.log(jwtToken);
+      let decodedJwt = jwt.verify(jwtToken, process.env.PRIVATE_KEY);
+      return decodedJwt;
+    } else {
+      // 토큰이 없는 경우
+      throw new ReferenceError("jwt must be provided");
+    }
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+    return err;
+  }
+};
 
-module.exports = decodeJwt
+module.exports = decodeJwt;
